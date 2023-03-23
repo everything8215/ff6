983408f0fb8f0653c0f68a3e4f80969a3192c6f5
Toussaint-Dupont, Frédéric <toussaint-dupont.frederic@courrier.uqam.ca>
2023-03-23 17:59:12 -0400

    Add sfx mml to bin function

--- tools/encode_mml.py
+++ tools/encode_mml.py
@@ -84,10 +84,76 @@ def akao_to_asm(data, channels, labels, filename):
 
     return asm_filename, asm_string
 
+def akao_sfx_to_asm(data, channels, filename):
+    # create sfx label and output filename
+    filename = os.path.splitext(filename)[0].lower()
+    label_elems = filename.split("_")
+    sfx_label = f'SfxScript_{label_elems[-1]}'
+    asm_filename = f'{filename}.asm'
+
+    # arrange data and ranges
+    data = data[0x26:len(data)]
+    ranges = []
+    ranges.append(rt.Range(0, channels[2] - 0x26))
+    if channels[2] != channels[3]:
+        ranges.append(rt.Range(channels[2] - 0x26, channels[3] - 0x26))
+
+    # file header
+    asm_string = '; this file is generated automatically,' \
+        + ' do not modify manually\n\n'
+    asm_string += '.list off\n'
+
+    # data
+    for i in range(len(ranges)):
+        channel = i + 1
+        asm_string += f'\n\n{sfx_label}_{channel}:'
+        data_block = data[ranges[i].begin:ranges[i].end]
+        for j in range(len(data_block)):
+            if j % 16 == 0:
+                asm_string += '\n        .byte   '
+            else:
+                asm_string += ','
+            asm_string += rt.hex_string(data_block[j], 2, '$').lower()
+    asm_string += '\n\n.list on\n'
+
+    return asm_filename, asm_string
+
 
 if __name__ == '__main__':
     asm_dir = os.path.join(os.getcwd(), 'include', 'script')
     mml_dir = os.path.join(os.getcwd(), 'script', 'mml')
+    sfx_dir = os.path.join(os.getcwd(), 'script', 'sfx')
+
+    for file in os.listdir(sfx_dir):
+        file_path = os.path.join(sfx_dir, file)
+        mml = []
+        # read mml file
+        try:
+            with open(file_path, 'r') as f:
+                mml = f.readlines()
+        except IOError as e:
+            print(f'Error reading file {file_path}')
+            print(e)
+
+        # convert mml file to binary and create asm string
+        try:
+            variants = mml_to_akao(mml)
+            def_variant = variants['_default_']
+            asm_filename, asm_string = akao_sfx_to_asm(def_variant[0],
+                                                        def_variant[1], file)           
+        except Exception:
+            traceback.print_exc()
+
+        # write asm file
+        try:
+            asm_path = os.path.join(asm_dir, asm_filename)
+            os.makedirs(os.path.dirname(asm_path), exist_ok=True)
+            with open(asm_path, 'w') as f:
+                f.write(asm_string)
+            print(f'{asm_filename} created.')
+        except IOError as e:
+            print(f'Error writing file {asm_path}')
+            print(e)
 
     for file in os.listdir(mml_dir):
         file_path = os.path.join(mml_dir, file)
